7b10149f7b79bd9e631907b97d797b9d3424635d
Timothy Hoang <timh1203@gmail.com>
2018-10-09 13:17:55 -0400

    Add POST and GET for courses route

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const helmet = require('helmet')
+const knex = require('knex')
 
+const knexConfig = require('./knexfile.js')
+const db = knex(knexConfig.development)
 const server = express()
 
 server.use(helmet())
@@ -10,7 +13,22 @@ server.get('/', (req, res) => {
   res.send("It's Alive")
 })
 
+// GET COURSES
+server.get('/api/courses', (req, res) => {
+  db('courses')
+    .then(courses => res.status(200).json(courses))
+    .catch(err => res.status(500).json(err))
+})
+
 // CREATE COURSES
+server.post('/api/courses', (req, res) => {
+  const course = req.body
+
+  db.insert(course)
+    .into('courses')
+    .then(ids => res.status(201).json(ids))
+    .catch(err => res.status(500).json(err))
+})
 
 // UPDATE COURSES
 
